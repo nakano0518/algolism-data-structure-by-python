abf11155298010b4af0d7db4fadb59e72104f4e7
nakano0518 <m15sc018.prog@gmail.com>
2021-05-21 15:50:20 +0000

    [mod] buble_sort

--- algo-data-structure2/03_buble_sort.py
+++ algo-data-structure2/03_buble_sort.py
@@ -1,14 +1,11 @@
 # バブルソート
-# リストの末尾から隣接要素を見ていき、大小関係が逆なら交換する
+# リストの末尾から隣接要素を見ていき(終点は1ずつ増える)、大小関係が逆なら交換する
 
 def buble_sort(arr):
-    for i in range(0, len(arr)-1):
-        exchange(arr, i)
-
-def exchange(arr, i):
-    for j in range(len(arr)-1, i, -1):
-        if arr[j-1] > arr[j]:
-            arr[j-1], arr[j] = arr[j], arr[j-1]
+    for i in range(0, len(arr)-1): # 末尾から見ていくときの終点
+        for j in range(len(arr)-1, i, -1): # 末尾から見ていく
+            if arr[j-1] > arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
 
 if __name__ == '__main__':
     org_list = [17 ,11, 12, 5, 14, 9, 6, 16, 4, 10, 1, 19, 13, 15, 0, 2, 3, 18, 7, 8]
